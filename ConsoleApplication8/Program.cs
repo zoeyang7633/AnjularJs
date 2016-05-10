00958e46d940639dabcccbf686dbedd89d3c12ee
zoeyang7633 <zoe7633@gmail.com>
2016-05-10 13:51:30 -0500

    hw

---
+++ ConsoleApplication8/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            input = input.Replace(" ",string.Empty);
+            char[] characters = input.ToCharArray();
+            bool yes = true;
+            for(int i=0;i<characters.Length/2;i++)
+            {
+                if (i == characters.Length / 2)
+                {
+                    
+                    break;
+                }
+                if (characters[i].ToString().ToUpper() != characters[characters.Length - 1 - i].ToString().ToUpper())
+                {
+                    yes = false;
+                    break;
+                }
+            }
+            Console.WriteLine(yes);
+
+        }
+    }
+}
